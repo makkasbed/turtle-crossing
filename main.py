c2eda61f9e2273034a001e5c710b3def33cdb133
adu <aluta182004@gmail.com>
2022-09-02 19:04:35 +0000

    working version

--- main.py
+++ main.py
@@ -34,6 +34,6 @@ while is_game_on:
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
-        scoreboard.update_scoreboard()
+        scoreboard.increase_level()
 
 screen.exitonclick()
